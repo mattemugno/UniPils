9863dcb510de33b9482e4e5385877e1d9aee1ab1
Buch97 <fpucciofilo97@gmail.com>
2022-02-06 17:17:01 +0100

    add classes in bean

--- UniPils/src/main/java/dii/unipi/lsmdb/bean/Beer.java
+++ UniPils/src/main/java/dii/unipi/lsmdb/bean/Beer.java
@@ -1,4 +1,18 @@
 package dii.unipi.lsmdb.bean;
 
 public class Beer {
+
+    private int id;
+    private String name;
+    private String state;
+    private String country;
+    private String style;
+    private int availability;
+    private int abv;
+    private int volume;
+    private int price;
+    private int brewery_id;
+    private String brewery_name;
+    private String brewery_city;
+    private int view_count;
 }
--- UniPils/src/main/java/dii/unipi/lsmdb/bean/User.java
+++ UniPils/src/main/java/dii/unipi/lsmdb/bean/User.java
@@ -1,4 +1,18 @@
 package dii.unipi.lsmdb.bean;
 
+import java.util.Date;
+
 public class User {
+
+    private String username;
+    private String password;
+    private String first;
+    private String last;
+    private char gender;
+    private String email;
+    private Date dob;
+    private int cell;
+
+
+
 }
