2edb4b3cbae4a0281ca63475701562f8b9a5bc02
stefanobianco12 <s.bianchettin@gmail.com>
2022-02-09 17:57:00 +0100

    Aggiunti homepage e menu su ogni pagina

---
+++ UniPils/src/main/java/it/unipi/lsmdb/controller/HomeController.java
@@ -0,0 +1,10 @@
+package it.unipi.lsmdb.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+
+public class HomeController {
+
+    @FXML private Parent embeddedMenu; //embeddedElement
+    @FXML private MenuController menuController;
+}
