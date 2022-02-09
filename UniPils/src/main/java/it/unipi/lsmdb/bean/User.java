c150a08270448839b6fa8d4221268979d117b68e
stefanobianco12 <96834783+stefanobianco12@users.noreply.github.com>
2022-02-09 01:00:05 +0100

    Add files via upload

--- UniPils/src/main/java/it/unipi/lsmdb/bean/User.java
+++ UniPils/src/main/java/it/unipi/lsmdb/bean/User.java
@@ -1,65 +1,107 @@
 package it.unipi.lsmdb.bean;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class User {
 
-    private String username;
-    private String password;
-    private String first;
-    private String last;
     private String gender;
+    private ArrayList<String> name;
     private String email;
+    private ArrayList<String> login;
     private LocalDate dob;
     private String cell;
+    private ArrayList<Order> orders;
+    private ArrayList<Payment> payments;
+    private String address;
 
-    public User(String username, String first, String last, String email, String gender, LocalDate dob, String cell ){
-        this.username=username;
-        this.first=first;
-        this.last=last;
+
+
+    public User(String gender, ArrayList<String> name, String email, ArrayList<String> login,
+                LocalDate dob, String cell,ArrayList<Order> orders,ArrayList<Payment> payments,String address){
+        this.name=name;
+        this.login=login;
+        this.orders=orders;
+        this.payments=payments;
+        this.address=address;
         this.email=email;
         this.gender=gender;
         this.dob=dob;
         this.cell=cell;
     }
 
-    public User(String username, String password){
-        this.username=username;
-        this.password=password;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getFirst(){
-        return first;
-    }
-
-    public String getLast(){
-        return last;
-    }
-    public String getCellular(){
-        return cell;
-    }
-
     public String getGender(){
         return gender;
     }
 
-    public LocalDate getDob(){
+    public ArrayList<String> getName() {
+        return name;
+    }
+
+    public ArrayList<String> getLogin() {
+        return login;
+    }
+
+    public LocalDate getDob() {
         return dob;
     }
 
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<Payment> getPayments() {
+        return payments;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCell() {
+        return cell;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(ArrayList<String> name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setLogin(ArrayList<String> login) {
+        this.login = login;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void setPayments(ArrayList<Payment> payments) {
+        this.payments = payments;
+    }
 
 
 }
