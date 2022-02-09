7080c27b90178cf22c8a58643294b94d14743f5a
stefanobianco12 <96834783+stefanobianco12@users.noreply.github.com>
2022-02-09 01:08:40 +0100

    Update

--- UniPils/src/main/java/it/unipi/lsmdb/persistence/MongoDriver.java
+++ UniPils/src/main/java/it/unipi/lsmdb/persistence/MongoDriver.java
@@ -1,10 +1,23 @@
 package it.unipi.lsmdb.persistence;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.mongodb.client.model.*;
+import it.unipi.lsmdb.bean.Beer;
 import it.unipi.lsmdb.bean.User;
 import it.unipi.lsmdb.config.InfoConfig;
 import com.mongodb.client.*;
 import com.mongodb.ConnectionString;
+import it.unipi.lsmdb.utils.Utils;
 import org.bson.Document;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.mongodb.client.model.Aggregates.sort;
+import static com.mongodb.client.model.Projections.*;
 
 public class MongoDriver {
 
@@ -19,21 +32,11 @@ public class MongoDriver {
 
     private static final ConnectionString uri= new ConnectionString(connectionString);
 
-    private static void openUserConnection(){
+    private static void openConnection(String nameCollection){
         try{
             myClient= MongoClients.create(uri);
             db= myClient.getDatabase(InfoConfig.getMongoDbName());
-            collection= db.getCollection("users");
-        }catch (Exception ex){
-            System.out.println("Impossible open connection with MongoDB");
-        }
-    }
-
-    private static void openBeersConnection(){
-        try{
-            myClient= MongoClients.create(uri);
-            db= myClient.getDatabase(InfoConfig.getMongoDbName());
-            collection= db.getCollection("beers");
+            collection= db.getCollection(nameCollection);
         }catch (Exception ex){
             System.out.println("Impossible open connection with MongoDB");
         }
@@ -46,6 +49,51 @@ public class MongoDriver {
         }catch (Exception ex){
             System.out.println("Impossible close connection with MongoDB");
         }
+    }
+
+    private static ArrayList<User> getBeanFromDocuments(ArrayList<Document> results){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ArrayList<User> users = new ArrayList<>();
+        try {
+            for (Document doc : results) {
+                User user = objectMapper.readValue(doc.toJson(), User.class);
+                users.add(user);
+            }
+
+            return users;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+
+    public static ArrayList<User> getBeersFromUsername(String username){
+        List<Document> results;
+
+        openConnection("User");
+        results=Arrays.asList(new Document("$unwind",
+                        new Document("path", "$orders")),
+                new Document("$unwind",
+                        new Document("path", "$orders.order_list")),
+                new Document("$match",
+                        new Document("login.username", "silversnake781")),
+                new Document("$project",
+                        new Document("orders.order_list", 1L)));
+        //Bson myMatch = Aggregates.match(Filters.eq("login.username", username));
+        //Bson mySort = sort(Sorts.descending("datePublished"));
+        //Bson projection = Aggregates.project( fields(excludeId(), include("recipeId","name", "authorName", "recipeCategory", "datePublished")));
+
+        //results = collection.aggregate(Arrays.asList(myMatch, mySort, projection))
+             //   .into(new ArrayList<>());
+        ArrayList<Document> res=new ArrayList<Document>(results);
+        closeConnection();
+        return getBeanFromDocuments(res);
     }
 
     public static boolean addUser(User u){
@@ -64,12 +112,14 @@ public class MongoDriver {
 
             collection.insertOne(doc);
 
-    }catch(Exception ex){
+        }catch(Exception ex){
+            closeConnection();
+            return false;
+        }
         closeConnection();
-        return false;
-    }
-    closeConnection();
         return true;
-}
+    }
+
+
 
 }
