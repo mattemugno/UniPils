0935ea54d91457b2236caa4a082fb0ed4d3c425b
Buch97 <fpucciofilo97@gmail.com>
2022-02-13 15:10:06 +0100

    nuova homepage aggiunta e admin finito

--- UniPils/src/main/java/it/unipi/lsmdb/HelloApplication.java
+++ UniPils/src/main/java/it/unipi/lsmdb/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("UniPils");
         stage.setScene(scene);
         stage.show();
     }
